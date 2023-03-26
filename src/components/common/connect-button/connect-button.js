f19743946f1a769d4348300913802868170bffe4
Petar Popovic <petarpnp@gmail.com>
2023-03-26 10:32:58 +0200

    Fix Rainbow Wallet hydration error

--- src/components/common/connect-button/connect-button.js
+++ src/components/common/connect-button/connect-button.js
@@ -2,61 +2,60 @@ import { ConnectButton } from "@rainbow-me/rainbowkit";
 import { Button } from "@chakra-ui/react";
 import WalletIcon from "@/components/common/icons/wallet";
 
+const renderButton = ({
+  account,
+  chain,
+  openAccountModal,
+  openChainModal,
+  openConnectModal,
+  authenticationStatus,
+  mounted
+}) => {
+  const connected =
+    account &&
+    chain &&
+    (!authenticationStatus ||
+      authenticationStatus === "authenticated");
+
+  if (!mounted) return;
+
+  if (!connected) {
+    return (
+      <Button
+        size="lg"
+        onClick={openConnectModal}
+      >
+        <WalletIcon width="20px" height="20px"/>
+      </Button>
+    );
+  }
+
+  if (chain.unsupported) {
+    return (
+      <Button
+        size="lg"
+        onClick={openChainModal}
+      >
+        Wrong network
+      </Button>
+    );
+  }
+
+  return (
+    <Button
+      size="lg"
+      onClick={openAccountModal}
+
+    >
+      {account.displayName}
+    </Button>
+  );
+}
+
 const CustomConnectButton = () => {
   return (
     <ConnectButton.Custom>
-      {({
-          account,
-          chain,
-          openAccountModal,
-          openChainModal,
-          openConnectModal,
-          authenticationStatus,
-        }) => {
-        const connected =
-          account &&
-          chain &&
-          (!authenticationStatus ||
-            authenticationStatus === "authenticated");
-
-        return (
-          <div>
-            {(() => {
-              if (!connected) {
-                return (
-                  <Button
-                    height={12}
-                    p={4}
-                    onClick={openConnectModal}
-                  >
-                    <WalletIcon width="20px" height="20px" />
-                  </Button>
-                );
-              }
-              if (chain.unsupported) {
-                return (
-                  <Button
-                    height={12}
-                    p={4}
-                    onClick={openChainModal}
-                  >
-                    Wrong network
-                  </Button>
-                );
-              }
-              return (
-                <Button
-                  height={12}
-                  p={4}
-                  onClick={openAccountModal}
-                >
-                  {account.displayName}
-                </Button>
-              );
-            })()}
-          </div>
-        );
-      }}
+      {(props) => renderButton(props)}
     </ConnectButton.Custom>
   );
 };
