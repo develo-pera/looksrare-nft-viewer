370e70f86c6eb17f3487c5ad62b0a13929f37059
Petar Popovic <petarpnp@gmail.com>
2023-03-26 10:38:56 +0200

    Link logo to homepage

--- src/components/common/navigation/navigation.js
+++ src/components/common/navigation/navigation.js
@@ -1,4 +1,6 @@
 import { Flex, Image } from "@chakra-ui/react";
+import NextLink from "next/link";
+import { Link } from "@chakra-ui/react";
 import ContentWrapper from "@/components/common/content-wrapper/contentWrapper";
 import CustomConnectButton from "@/components/common/connect-button/connect-button";
 
@@ -9,11 +11,14 @@ const Navigation = () => {
         justifyContent="space-between"
         alignItems="center"
       >
-        <Image
-          width="120px"
-          src="/logo.svg"
-          alt="LooksRare NFT viewer logo"
-        />
+
+        <Link as={NextLink} href='/'>
+          <Image
+            width="120px"
+            src="/logo.svg"
+            alt="LooksRare NFT viewer logo"
+          />
+        </Link>
         <CustomConnectButton />
       </Flex>
     </ContentWrapper>
